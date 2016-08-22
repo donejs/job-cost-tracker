9663396cc1a757bb6d6058a8b97faa87811e413b
Garrett Reed <garrett@garrettreed.co>
2016-08-22 15:36:41 -0500

    Add new test structure

--- public/test/functional.js
+++ public/test/functional.js
@@ -1,27 +1,14 @@
-import F from 'funcunit';
-import QUnit from 'steal-qunit';
-import 'job-tracker/models/fixtures/';
-
-F.attach(QUnit);
-
-QUnit.module('uses modern QUnit style', {
-  beforeEach: function() {
-    this.beforeValueOnContext = true;
-  }
-}, function() {
-
-  QUnit.test('runs nested it\'s propery', function(assert){
-    assert.ok(this.beforeValueOnContext);
-  });
-
-});
-
-// QUnit.module('job-tracker functional smoke test', {
-//   beforeEach() {
-//     F.open('../development.html');
-//   }
-// });
-//
-// QUnit.test('job-tracker main page shows up', function() {
-//   F('title').text('job-tracker', 'Title is set');
-// });
+// import 'job-tracker/components/welcome-message.component';
+// import 'job-tracker/components/app-nav/app-nav_test';
+// import 'job-tracker/components/tasks/tasks_test';
+// import 'job-tracker/components/task-day/task-day_test';
+// import 'job-tracker/components/job-reports/job-reports_test';
+// import 'job-tracker/components/new-lot/new-lot_test';
+// import 'job-tracker/components/new-task-day/new-task-day_test';
+// import 'job-tracker/components/custom-work-order/custom-work-order_test';
+// import 'job-tracker/components/dynamic-grid/dynamic-grid_test';
+// import 'job-tracker/components/date-range-picker/date-range-picker_test';
+// import 'job-tracker/components/new-task-day/tasks-completed/tasks-completed_test';
+// import 'job-tracker/components/autocomplete/autocomplete_test';
+// import 'job-tracker/components/user-login/user-login_test';
+// import 'job-tracker/components/user-manager/user-manager_test';
--- public/test/unit.js
+++ public/test/unit.js
@@ -1,8 +1,4 @@
-// import 'job-tracker/models/fixtures/';
-//
-// // import 'job-tracker/models/test';
-// import 'job-tracker/test/functional';
-//
+import '../components/app-nav/test/unit';
 // import 'job-tracker/components/welcome-message.component';
 // import 'job-tracker/components/app-nav/app-nav_test';
 // import 'job-tracker/components/tasks/tasks_test';
@@ -17,6 +13,3 @@
 // import 'job-tracker/components/autocomplete/autocomplete_test';
 // import 'job-tracker/components/user-login/user-login_test';
 // import 'job-tracker/components/user-manager/user-manager_test';
-
-
-import "steal-mocha";
