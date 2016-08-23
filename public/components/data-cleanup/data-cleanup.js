db0d791a22d1e5649b40b2793484838dadea5f59
Julian <julian@Julians-MacBook-Pro.local>
2016-08-23 18:39:55 +0200

    unit test for data-cleanup

--- public/components/data-cleanup/data-cleanup.js
+++ public/components/data-cleanup/data-cleanup.js
@@ -1,16 +1,12 @@
 import Component from 'can/component/';
 import Map from 'can/map/';
 import 'can/map/define/';
-import './data-cleanup.less!';
-import template from './data-cleanup.stache!';
+import './data-cleanup.less';
+import template from './data-cleanup.stache';
 import moment from 'moment';
 
 export const ViewModel = Map.extend({
-  define: {
-    message: {
-      value: 'This is the data-cleanup component'
-    }
-  }
+  define: {}
 });
 
 export default Component.extend({
