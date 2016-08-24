06481912ed2b8f7523daa7c49dd9680b369f047e
Garrett Reed <garrett@garrettreed.co>
2016-08-24 14:53:23 -0500

    Complete <header> component docs and implementation

--- public/components/header/header.js
+++ public/components/header/header.js
@@ -4,9 +4,20 @@ import 'can/map/define/';
 import './header.less!';
 import template from './header.stache!';
 
+/**
+ * @module {Module} job-tracker/components/header <header>
+ * @parent job-tracker.components
+ *
+ * @description The application static header
+ *
+ * @signature `<header />`
+ *
+ * @demo public/components/header/header.html
+ *
+ **/
 export const ViewModel = Map.extend({
   define: {
-  },
+  }
 });
 
 export default Component.extend({
