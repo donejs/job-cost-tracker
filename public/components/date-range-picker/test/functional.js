2d8008d7e761286e442e853abd2ebe5552fea9f8
Julian <julian@Julians-MacBook-Pro.local>
2016-08-25 16:35:22 -0500

    updated date-range-picker

--- public/components/date-range-picker/test/functional.js
+++ public/components/date-range-picker/test/functional.js
@@ -2,26 +2,33 @@ import chai from "chai";
 import F from 'funcunit';
 import mocha from "steal-mocha";
 
-import canStache from "can/view/stache/";
 import '../date-range-picker';
 import moment from "moment";
 
 const assert = chai.assert;
 
-F.attach(mocha);
 
-describe('pikaday', function(){
-	beforeEach(function(){
+describe("<date-range-picker>", function(){
+
+	before(function () {
+		F.attach(mocha);
+	});
+
+	after(function () {
+		F.detach(mocha);
+	});
+
+	beforeEach(function(done){
 		F.open('/public/components/date-range-picker/date-range-picker.html');
-		// wait until the component is append by done-autorender
-		F('date-range-picker').exists();
+		F('date-range-picker').exists(10000, function () {
+			// wait until the component is append by done-autorender
+			done()
+		})
 	});
 
 	it('for the end date', function(done){
 
 		F(".report-date-end:eq(0)").click(function(){
-			F("div.pika-single>div.pika-lendar").exists();
-
 			let lastDayInFirstWeek = F('table.pika-table>tbody>tr').first().find('td').last().find('button');
 
 			let day = lastDayInFirstWeek.attr("data-pika-day");
@@ -35,13 +42,13 @@ describe('pikaday', function(){
 				assert.equal(inputEndDate, calcEndDate);
 			});
 		});
-
 		F.add(done);
 	});
 
 
 	it('for the start date', function(done){
-		F(".report-date-start:eq(0)").click(function(){
+
+		F(".report-date-start:eq(0)").click(function () {
 			F("div.pika-single>div.pika-lendar").exists();
 
 			let lastDayInFirstWeek = F('table.pika-table>tbody>tr').first().find('td').last().find('button');
