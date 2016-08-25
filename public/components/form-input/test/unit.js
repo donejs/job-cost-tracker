c39025ee56c9b8e56c52373ace3991ec6f9f9f97
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2016-08-25 15:36:17 -0500

    Upgrade components/form-input
    Closes #67

--- public/components/form-input/test/unit.js
+++ public/components/form-input/test/unit.js
@@ -7,20 +7,21 @@ const assert = chai.assert;
 describe("form-input Component ViewModel", function () {
   it('has a type', function() {
     var vm = new ViewModel();
-    assert.equal(vm.attr('type'), 'text', 'it is "text"');
+    assert.equal(vm.type, 'text', 'it is "text"');
   });
 
 	it("has a random id", function() {
 		var vm = new ViewModel();
-		assert.typeOf(vm.attr('randomId'), 'number', 'it is a number');
+		assert.typeOf(vm.randomId, 'number', 'it is a number');
+
     var vmAlt = new ViewModel();
-    assert.notEqual(vm.attr('randomId'), vmAlt.attr('randomId'), 'it is unique to a vm');
+    assert.notEqual(vm.randomId, vmAlt.randomId, 'it is unique to a vm');
 	});
 
   it("has a unique id", function() {
     var vm = new ViewModel();
-    var rand = vm.attr('randomId');
-    assert.equal(vm.attr('uniqueId'), `text-input-${rand}`, 'it is a string combination of randomId and type');
-  });
+    var rand = vm.randomId;
 
+    assert.equal(vm.uniqueId, `text-input-${rand}`, 'it is a string combination of randomId and type');
+  });
 });
